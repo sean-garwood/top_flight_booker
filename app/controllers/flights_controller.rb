7f76e7a08fa15ffc8a1205bd27900fa2d062c3d9
sean-garwood <seansgarwood@gmail.com>
2024-09-30 16:06:06 -0400

    fix: partials for _search, _results

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,7 @@
 class FlightsController < ApplicationController
   def index
+    # TODO: filter flights by results from search form
     @flights = Flight.all
+    @airports = Airport.all.map { |airport| [ airport.code, airport.id ] }
   end
 end
