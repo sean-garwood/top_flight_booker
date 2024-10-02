67c2ebcbb5518b5fbea7c48276c0a4830ccd5c85
sean-garwood <seansgarwood@gmail.com>
2024-10-02 14:41:13 -0400

    fix: search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,12 @@
 class FlightsController < ApplicationController
   def index
-    # TODO: filter flights by results from search form
-    @flights = Flight.all
     @airports = Airport.all.map { |airport| [ airport.code, airport.id ] }
+
+    if params[:departure_airport].present? && params[:arrival_airport].present?
+      @flights = Flight.joins(:departure, :arrival)
+                       .where(departures: { airport_id: params[:departure_airport] }, arrivals: { airport_id: params[:arrival_airport] })
+    else
+      @flights = Flight.all
+    end
   end
 end
