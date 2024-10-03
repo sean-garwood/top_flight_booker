43850cfd5e17d2b51d6277d4b719d42729dfb7b7
sean-garwood <seansgarwood@gmail.com>
2024-10-03 06:08:48 -0400

    fix: date, passenger dropdowns
    * passengers limited 1-4 based on spec
    * only include dates that have flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,10 +2,11 @@ class FlightsController < ApplicationController
   def index
     @airports = Airport.all.pluck(:code, :id)
     # filter flights by:
-    #   departure airport
+    #  departure airport
     #  arrival airport
     #  available seats
     #  departure date (not time)
+    @departure_dates = get_departure_dates
     @flights = Flight.search_by_airports(
                flight_params[:departure_airport], flight_params[:arrival_airport]).
                search_by_date(flight_params[:depart_date]).
@@ -13,6 +14,12 @@ class FlightsController < ApplicationController
   end
 
   private
+    def get_departure_dates
+      # get all departure dates for all flights
+      dates = Flight.pluck(:depart_time).map { |date| date.to_date }.uniq
+      dates.map { |date| [ date.strftime("%m/%d/%Y"), date ] }.sort
+    end
+
     def flight_params
       params.require(:flight)
         .permit(:depart_date, :available_seats,
