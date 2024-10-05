b28ab49d4057dab19c17f9128550899d1180c763
sean-garwood <seansgarwood@gmail.com>
2024-10-05 17:09:30 -0400

    fix: deuglify url params
    FIXIT: kind of hacky:
    * Not using strong params (require/permit)
    * N+1 query?

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,28 +1,26 @@
 class FlightsController < ApplicationController
   def index
     @airports = Airport.all.pluck(:code, :id)
-    # filter flights by:
-    #  departure airport
-    #  arrival airport
-    #  available seats
-    #  departure date (not time)
     @departure_dates = get_departure_dates
     @flights = Flight.search_by_airports(
                flight_params[:departure_airport], flight_params[:arrival_airport]).
                search_by_date(flight_params[:depart_date]).
-                search_by_passengers(flight_params[:available_seats])
+               search_by_passengers(flight_params[:available_seats])
   end
 
   private
-    def get_departure_dates
-      # get all departure dates for all flights
-      dates = Flight.pluck(:depart_time).map { |date| date.to_date }.uniq
-      dates.map { |date| [ date.strftime("%m/%d/%Y"), date ] }.sort
-    end
 
-    def flight_params
-      params.require(:flight)
-        .permit(:depart_date, :available_seats,
-                :departure_airport, :arrival_airport)
-    end
+  def get_departure_dates
+    dates = Flight.pluck(:depart_time).map { |date| date.to_date }.uniq
+    dates.map { |date| [ date.strftime("%m/%d/%Y"), date ] }.sort
+  end
+
+  def flight_params
+    {
+      departure_airport: params[:departure_airport],
+      arrival_airport: params[:arrival_airport],
+      available_seats: params[:available_seats],
+      depart_date: params[:depart_date]
+    }
+  end
 end
