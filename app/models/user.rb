32b2030b9188190c0f2dd2af8c76211bf492a385
sean-garwood <seansgarwood@gmail.com>
2024-10-14 14:23:48 -0400

    fixup! user.bookings, data turbo

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :bookings
+  has_many :bookings, class_name: "Booking", foreign_key: "passenger_id"
   has_many :flights, through: :bookings
 end
