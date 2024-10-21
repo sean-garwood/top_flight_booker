4f9ef7dce4cdd3c5080ece0c720e1edc13be3da4
sean-garwood <seansgarwood@gmail.com>
2024-10-21 12:11:57 -0400

    fix: bookings_controller
    * rework booking.passenger to remove devise behavior

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,25 +1,26 @@
 class BookingsController < ApplicationController
   def new
     @flight = Flight.find(booking_params[:flight_id])
-    @booking = Booking.new(booking_params)
+    @booking = Booking.new
+    booking_params[:number_of_tickets].to_i.times { @booking.passengers.build }
   end
 
   def create
     @booking = Booking.new(booking_params)
-    @booking.passenger = current_user
+    @booking.flight = Flight.find(booking_params[:flight_id])
     if @booking.save
       flash[:notice] = "Booking created!"
       redirect_to root_path
     else
       flash[:alert] = "Booking failed!"
-      redirect_to root_path, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(
+    params.fetch(:booking, {}).permit(
       :flight_id,
       :number_of_tickets,
       passengers_attributes: [ :name, :email ])
