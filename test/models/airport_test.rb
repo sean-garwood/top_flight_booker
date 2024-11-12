0c7911a152594ea0064aec81a2a319afcc3b9924
sean-garwood <seansgarwood@gmail.com>
2024-11-12 15:22:14 -0500

    fix: remove bad tests

--- test/models/airport_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class AirportTest < ActiveSupport::TestCase
-  test "should not save airport without code" do
-    airport = Airport.new
-    assert_not airport.save, "Saved the airport without a code"
-  end
-end
